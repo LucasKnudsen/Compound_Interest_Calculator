ba40e208f0015ae241c6c7b155f20ee84f25601a
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-12 23:14:06 +0100

    Implements 4 input fields. Forgot TDD. On to that now

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Compounding Interest Calculator
 ## Hobby project
+User can type in details in order to calculate his/her profit.
 
 ### User Stories
 ```
-
+As a user
+In order to type in my details
+I need a form with input fields
+```
+```
+As a user
+When I have typed in my details
+I want to receive my potential profit
 ```
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { Form, Row, Col } from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    start: 0,
+    monthly: 0,
+    rate: 0,
+    time: 0,
+  }
+  calculateProfit = () => {
+
+  }
+
+  render() {
+
+
+    return (
+      <div>
+        <Form>
+          <Row>
+            <Col>
+              <Form.Control placeholder="Startbelob" />
+            </Col>
+            <Col>
+              <Form.Control placeholder="Månedlig opsparing" />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Control placeholder="Årligt afkast" />
+            </Col>
+            <Col>
+              <Form.Control placeholder="Tidshorisont" />
+            </Col>
+          </Row>
+        </Form>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+/*
+Compounded interest + future value of series
+P = Principal investment
+PMT = Principal Monthly
+r = interest rate
+n = number of times interest is compounded
+t = the time
+
+( P(1+r/n)**(n*t) ) + ( PMT * (((1+r/n)**(n*t)-1) / (r/n)) )
+
+*/

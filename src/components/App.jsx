fcc6f921ce47505853d599c80773dc702286ffdc
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-13 00:05:47 +0100

    Compounding interes formula implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import { Form, Row, Col } from 'react-bootstrap'
 
 const App = () => {
 
-  const [start, setStart] = useState(0)
-  const [monthly, setMonthly] = useMeState(0)
-  const [rate, setRate] = useState(0)
-  const [time, setTime] = useState(0)
-  const [result, setResult] = useState(0)
+  const [start, setStart] = useState(10000)
+  const [monthly, setMonthly] = useState(100)
+  const [rate, setRate] = useState(0.05)
+  const [time, setTime] = useState(10)
+  const [profit, setProfit] = useState(0)
 
-  calculateProfit = () => {
+  const calculateProfit = () => {
     const n = 12
-
+    let result = (start * (1 + rate/n) ** (n * time)) + (monthly * (((1 + rate / n) ** (n * time) - 1) / (rate / n)))
+    setProfit(result.toFixed(0))
   }
+
+  useEffect(() => {
+    calculateProfit()
+  })
+
+  /*
+Compounded interest + future value of series
+P = Principal investment
+PMT = Principal Monthly
+r = interest rate
+n = number of times interest is compounded
+t = the time
+
+( P(1+r/n)**(n*t) ) + ( PMT * (((1+r/n)**(n*t)-1) / (r/n)) )
+
+*/
 
   return (
     <div>
@@ -35,7 +52,7 @@ const App = () => {
           </Col>
         </Row>
         <Row className="justify-content-md-center">
-          <h1></h1>
+          <h1>{profit}</h1>
         </Row>
       </Form>
     </div>
@@ -45,14 +62,4 @@ const App = () => {
 
 export default App;
 
-/*
-Compounded interest + future value of series
-P = Principal investment
-PMT = Principal Monthly
-r = interest rate
-n = number of times interest is compounded
-t = the time
 
-( P(1+r/n)**(n*t) ) + ( PMT * (((1+r/n)**(n*t)-1) / (r/n)) )
-
-*/
